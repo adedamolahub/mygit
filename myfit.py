c9a21273b7d88d5ecc6ab0ea23c3d1103b5e2066
Adedamola <Adedamolamalik@gmail.com>
2025-01-21 21:27:48 +0000

    push

--- myfit.py
+++ myfit.py
@@ -3,5 +3,7 @@ for i in range(5):
     print("hello, world!")
     print("hello, world1!")
     print("hello, world!")
+    print("hello, world!")
+    print("hello123, world!")
     print(i + 1)
     print(i + 1)
